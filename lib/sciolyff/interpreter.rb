a45fe286381f9764a0e9a105c0feb38267ba5f8e
Em Zhan <zqianem@gmail.com>
2019-11-03 11:53:47 -0500

    Add interpreter support for subdivisions
    Incorrectly ignores/overrides ties within divisions for now

--- lib/sciolyff/interpreter.rb
+++ lib/sciolyff/interpreter.rb
@@ -13,13 +13,20 @@ module SciolyFF
     attr_reader :tournament, :events, :teams, :placings, :penalties
 
     def initialize(rep)
-      create_models(rep)
+      create_models(@rep = rep)
       link_models(self)
 
       sort_events_naturally
       sort_teams_by_rank
 
       freeze_models
+    end
+
+    def subdivisions
+      teams.map(&:subdivision)
+           .uniq
+           .compact
+           .map { |sub| Interpreter.new(subdivision_rep(sub)) }
     end
 
     private
@@ -90,6 +97,29 @@ module SciolyFF
             .zip(team_b.trial_event_medal_counts)
             .map { |counts| counts.last - counts.first }
             .find(proc { team_a.number <=> team_b.number }, &:nonzero?)
+    end
+
+    def subdivision_rep(sub)
+      # make a deep copy of rep and remove teams not in the subdivision
+      rep = Marshal.load(Marshal.dump(@rep))
+      rep[:Teams].select! { |t| t[:subdivision] == sub }
+
+      team_numbers = rep[:Teams].map { |t| t[:number] }
+      rep[:Placings].select! { |p| team_numbers.include? p[:team] }
+
+      fix_placings_for_existing_teams(rep)
+      rep
+    end
+
+    def fix_placings_for_existing_teams(rep)
+      rep[:Placings]
+        .group_by { |p| p[:event] }
+        .each do |_, event_placings|
+        event_placings
+          .select { |p| p[:place] }
+          .sort_by { |p| p[:place] }
+          .each_with_index { |p, i| p[:place] = i + 1 }
+      end
     end
   end
 end
