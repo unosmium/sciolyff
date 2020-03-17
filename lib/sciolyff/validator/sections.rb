04ea25d3d2b29483c2b9bfec2156f91c26baed30
Em Zhan <zqianem@gmail.com>
2020-03-17 13:52:14 -0400

    Add proper type checks

--- lib/sciolyff/validator/sections.rb
+++ lib/sciolyff/validator/sections.rb
@@ -3,36 +3,37 @@
 module SciolyFF
   # Top-level sections of a SciolyFF file
   module Validator::Sections
-    REQUIRED = %i[
-      Tournament
-      Events
-      Teams
-      Placings
-    ].freeze
+    REQUIRED = {
+      Tournament: Hash,
+      Events: Array,
+      Teams: Array,
+      Placings: Array
+    }.freeze
 
-    OPTIONAL = %i[
-      Penalties
-    ].freeze
+    OPTIONAL = {
+      Penalties: Array
+    }.freeze
 
     def all_required_sections?(rep, logger)
-      missing_sections = REQUIRED - rep.keys
+      missing_sections = REQUIRED.keys - rep.keys
       return true if missing_sections.empty?
 
       logger.error "missing required sections: #{missing_sections.join ', '}"
     end
 
     def no_extra_sections?(rep, logger)
-      extra_sections = rep.keys - (REQUIRED + OPTIONAL)
+      extra_sections = rep.keys - (REQUIRED.keys + OPTIONAL.keys)
       return true if extra_sections.empty?
 
       logger.error "extra section(s) found: #{extra_sections.join ', '}"
     end
 
-    def sections_are_lists?(rep, logger)
+    def sections_are_correct_type?(rep, logger)
       rep.all? do |key, value|
-        next true if value.instance_of? Array
+        correct_type = (REQUIRED.merge OPTIONAL)[key]
+        next true if value.instance_of? correct_type
 
-        logger.error "section is not a list: #{key}"
+        logger.error "section is not #{correct_type}: #{key}"
       end
     end
   end
