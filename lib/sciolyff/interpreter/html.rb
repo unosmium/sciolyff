46b8dd6c7f58bacc5126bfdc3d94d5250db0cdf5
Em Zhan <zqianem@gmail.com>
2020-04-07 17:59:47 -0400

    Embed original SciolyFF YAML to end of HTML

--- lib/sciolyff/interpreter/html.rb
+++ lib/sciolyff/interpreter/html.rb
@@ -8,7 +8,28 @@ module SciolyFF
 
     def html
       helpers = Interpreter::HTML::Helpers.new
-      ERB.new(helpers.template).result(helpers.get_binding(self))
+      ERB.new(helpers.template).result(helpers.get_binding(self)) +
+        "<yaml hidden>\n#{yaml}</yaml>"
+    end
+
+    def yaml
+      stringify_keys(@rep).to_yaml
+    end
+
+    private
+
+    def stringify_keys(hash)
+      return hash unless hash.instance_of? Hash
+
+      hash.map do |k, v|
+        new_k = k.to_s
+        new_v = case v
+                when Array then v.map { |e| stringify_keys(e) }
+                when Hash then stringify_keys(v)
+                else v
+                end
+        [new_k, new_v]
+      end.to_h
     end
   end
 end
